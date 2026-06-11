e91ef1beb29dc16f83140eb50c346a27835a0559
bee-honey <honeybeeprogrammer@gmail.com>
2026-06-11 14:40:56 -0700

    Few fixes with the application.yml for setting spring boot ready

---
+++ guardian-server/src/main/java/io/iamguardian/server/controllers/HealthController.java
@@ -0,0 +1,12 @@
+package io.iamguardian.server.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "IAM Guardian is running";
+    }
+}
