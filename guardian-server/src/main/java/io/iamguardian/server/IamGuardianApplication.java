c348e38ff61b5925a27175224a80cf3bc3bcfb30
bee-honey <honeybeeprogrammer@gmail.com>
2026-06-11 11:20:58 -0700

    Initialize IAM Guardian project and architecture docs

---
+++ guardian-server/src/main/java/io/iamguardian/server/IamGuardianApplication.java
@@ -0,0 +1,13 @@
+package io.iamguardian.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamGuardianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamGuardianApplication.class, args);
+    }
+
+}
---
+++ guardian-server/src/test/java/io/iamguardian/server/IamGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package io.iamguardian.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamGuardianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
